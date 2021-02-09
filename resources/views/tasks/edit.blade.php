d8aa255a51a4c6c213b35f2a18f6950e0a9e7771
Behruz Muyassarov <muyassarov@gmail.com>
2021-02-09 15:03:26 +0500

    added ability to unselect labels in task editing

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,7 +9,7 @@
     {{ Form::bsTextarea('description', $task->description, __('forms.task.edit.description-label'), ['placeholder' => __('forms.task.edit.description-placeholder'), 'rows' => 2]) }}
     {{ Form::bsSelect('status_id', $taskStatuses, $task->status_id, __('forms.task.edit.status-label'), ['required']) }}
     {{ Form::bsSelect('assigned_to_id', $users, $task->assigned_to_id, __('forms.task.edit.assigned-to-label'), ['placeholder' => __('forms.task.edit.assigned-to-placeholder')]) }}
-    {{ Form::bsSelect('labels[]', $labels, $task->labels->pluck('id'), __('forms.task.edit.labels-label'), ['multiple']) }}
+    {{ Form::bsSelect('labels[]', $labels, $task->labels->pluck('id'), __('forms.task.edit.labels-label'), ['multiple', 'placeholder' => __('forms.task.edit.labels-placeholder')]) }}
     {{ link_to_route('tasks.index', __('forms.task.edit.back'), [], ['class' => 'btn btn-lg btn-secondary']) }}
     {{ Form::submit(__('forms.task.edit.submit'), ['class' => 'btn btn-primary btn-lg']) }}
     {{ Form::close() }}
